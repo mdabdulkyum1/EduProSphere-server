9e738e6d6503dc175ac209f1d4de65b335a0f22b
mdabdulkyum1 <kyummdabdul@gmail.com>
2025-01-19 23:27:42 +0600

    ready to deploy

--- index.js
+++ index.js
@@ -12,11 +12,6 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
-
-
-
-
 const uri = `mongodb+srv://${process.env.USER_DB}:${process.env.PASS_DB}@cluster0.kzmhu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -400,8 +395,8 @@ app.get('/all-feedbacks', async (req, res)=> {
 
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -412,7 +407,7 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res)=> {
-    res.send("final project running...");
+    res.send("assignment 12 server running...");
 })
 app.listen(port, ()=>{
     console.log(`server running on PORT: ${port}`);
